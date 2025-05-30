cc3e05c5e99641cccb4e97355ee95762b4ae1e64
Omar <omarguaba54@gmail.com>
2025-05-30 12:53:07 -0400

    movie details almost done

---
+++ src/components/MediaListItem.tsx
@@ -0,0 +1,25 @@
+import { MediaListData } from "@/types/types";
+import { Image, Pressable  } from "react-native"
+import { Link } from "expo-router"
+
+
+type MediaListItemProps = {
+  mediaItem: MediaListData;
+}
+
+export default function MediaListItem({ mediaItem }: MediaListItemProps) {
+
+
+  
+
+  return (
+
+   <Link href={`mediaDetails/${mediaItem.id}`} asChild>
+      <Pressable>
+        <Image source={{ uri: mediaItem.image }} style={{ width: 110, aspectRatio: 3 / 4, borderRadius: 5, marginHorizontal: 4 }} />
+      </Pressable>
+    </Link>
+  
+  
+  )
+}
